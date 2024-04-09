1eeb5a0637093a8ed84c7efa7c950ed3d54416a1
Collin Koldoff <collinkoldoff@gmail.com>
2024-04-09 14:32:54 -0500

    Updated home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,39 @@
-import React from 'react';
-import { Button, Col, Container, Row } from 'react-bootstrap';
-import { Example } from '../components';
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import { Container } from 'react-bootstrap';
+import { ClassSection } from 'src/models';
 
 function Home() {
-    return (
-        <Container>
-          <Row>
-            <Col lg={3} md={4}>
-              <Button>Test</Button>
-              <Example />
-            </Col>
-          </Row>
-        </Container>
-    )
+  const [sections, setSections] = React.useState<ClassSection[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      await axios.get(`${process.env.REACT_APP_API_URL}/api/csci/`).then((response) => {
+        setSections(response.data);
+      }).catch((error) => {
+        console.error(error);
+      });
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <Container>
+      {sections.length === 0 ? <h2>Loading...</h2> :
+        <>
+        <h2>Available Lewis University Syllibi</h2>
+          <ol>
+            {sections.map((section, index) => (
+              <li>
+                <a href={`/syllabus/${section.id}`}>{section.id}</a>
+              </li>
+            ))}
+          </ol>
+        </>
+      }
+    </Container>
+  )
 }
 
 export default Home;
